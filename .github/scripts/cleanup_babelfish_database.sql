15cbb36ec147a7ca2c8841ce670853883b48f478
alex <alex@staticlibs.net>
2023-03-24 21:06:31 +0000

    drop github actions

--- .github/scripts/cleanup_babelfish_database.sql
+++
@@ -1,8 +0,0 @@
-\c :db
-CALL SYS.REMOVE_BABELFISH();
-ALTER SYSTEM RESET babelfishpg_tsql.database_name;
-SELECT pg_reload_conf();
-\c postgres
-DROP DATABASE :db WITH (FORCE);
-DROP OWNED BY :user;
-DROP USER :user;
